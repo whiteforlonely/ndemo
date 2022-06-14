0143a869743368a1ab80dc36c250317e63096328
keqingyuan <695774301@qq.com>
2022-06-14 09:04:17 +0800

    init netty in action demo

---
+++ src/main/java/com/ake/netty/action/ch1/EchoServerHandler.java
@@ -0,0 +1,17 @@
+package com.ake.netty.action.ch1;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        super.channelRead(ctx, msg);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        super.exceptionCaught(ctx, cause);
+    }
+}
